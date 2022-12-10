65651383e0752a73acf3593e3baa2d748c9be503
TobyDyson123 <p110138434@student.peterborough.ac.uk>
2022-12-10 16:09:50 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import logo_black from './images/logo-black.png';
+import {useRef} from 'react';
+import './carousel.js';
+import Carousel from './carousel';
+import './carousel.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const ref = useRef(null);
+
+  const scrollToBegin = () => {
+    ref.current?.scrollIntoView({behavior: 'smooth'});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-container">
+        <div className="logo-container">
+          <img className="logo" src={logo_black} alt="get baking logo"></img>
+        </div>
+        <div className="about-container">
+          <p className="about-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Nam maximus ornare felis, eu lobortis odio. 
+            Integer eget semper arcu, sit amet tincidunt mi. 
+            Nulla tortor erat, tristique at vestibulum sit amet, vestibulum ut felis. 
+            Nulla accumsan sapien vitae felis lacinia commodo. 
+            Nulla justo sem, placerat nec tempor at, vulputate non urna. 
+            Nunc id pellentesque ligula. Mauris euismod arcu at enim imperdiet, sit amet blandit neque cursus. 
+          </p>
+        </div>
+        <div className="begin-button-container">
+          <button className="begin-button" onClick={scrollToBegin}>Let's Begin</button>
+        </div>
+        <div class="mouse_scroll">
+          {/* <div class="mouse">
+            <div class="wheel"></div>
+          </div> */}
+          <div>
+            <span class="m_scroll_arrows unu"></span>
+            <span class="m_scroll_arrows doi"></span>
+            <span class="m_scroll_arrows trei"></span>
+          </div>
+        </div>
+      </div>
+      <div ref={ref} className="nav-container">
+        <button>Explore Recipes</button>
+        <button>Use up your Leftovers</button>
+        <button>Build Custom Recipes</button>
+      </div>
+      <div className="main-content">
+        <div className="recent-recipes-container">
+          <Carousel />
+        </div>
+      </div>
+      <div className="footer">
+        <div className="footer-container">
+          THIS IS THE FOOTER!
+        </div>
+      </div>
     </div>
   );
 }
