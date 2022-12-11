0448494212911279fb9b698f866d83d1669e2867
TobyDyson123 <tobyrdyson@ntlworld.com>
2022-12-11 16:10:42 +0000

    changed how the carousels looked

--- src/carousel.js
+++ src/carousel.js
@@ -46,4 +46,48 @@ function RecentRecipesCarousel() {
   );
 }
 
+function PopularCarousel() {
+  return (
+    <Carousel variant="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={bread}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={cake}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={bread}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
 export default RecentRecipesCarousel;
--- src/main-content.js
+++ src/main-content.js
@@ -1,13 +1,18 @@
 import './carousel.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RecentRecipesCarousel from './carousel.js'
+import RecentRecipesCarousel from './carousel.js';
+import PopularCarousel from './carousel.js';
 
 function MainContent() {
     return (
         <div className="main-content">
-            <div className="recent-recipes-container">
-                <h1 className='recent-recipes-title'>Recent Recipes</h1>
+            <div className="recipes-carousel-container">
+                <h1 className='recipes-title'>Recent Recipes</h1>
                 <RecentRecipesCarousel />
+            </div>
+            <div className="recipes-carousel-container">
+                <h1 className='recipes-title'>Popular Recipes</h1>
+                <PopularCarousel />
             </div>
         </div>
     );
