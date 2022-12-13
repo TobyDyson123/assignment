e528e52d1b7375c0c38fa4436be98411add4a959
TobyDyson123 <tobyrdyson@ntlworld.com>
2022-12-13 22:51:38 +0000

    made navbar responsive

--- src/navbar.js
+++ src/navbar.js
@@ -1,6 +1,23 @@
 function Navbar() {
+    const menuClick = () => {
+        var elements = document.getElementById("begin-scroll").children;
+        for (var i = 0; i < elements.length; i++) {
+            var child = elements[i];
+            if (child === document.getElementById("menu-icon")) {
+                continue;
+            }
+            if (child.style.display === "none") {
+                child.style.display = "block";
+            } else {
+                child.style.display = "none";
+            }
+            
+        }
+    };
+
     return (
         <div className="nav-container" id="begin-scroll">
+            <button id="menu-icon" onClick={menuClick}>Menu -</button>
             <button>Explore Recipes</button>
             <button>Use up your Leftovers</button>
             <button>Build Custom Recipes</button>
