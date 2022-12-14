c211f9d6e315e36670fe871a17ae1a82a30b2304
TobyDyson123 <tobyrdyson@ntlworld.com>
2022-12-14 14:55:20 +0000

    added scroll to top button

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './title.css';
 import './main-content.css';
 import './navbar.css';
 import './footer.css';
+import './scrolltotop.css';
 import Title from './title';
 import MainContent from './main-content';
 import Navbar from './navbar';
 import Footer from './footer';
+import ScrollButton from './scrolltotop';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <Navbar />
+      <ScrollButton />      
       <MainContent />
       <Footer />
     </div>
--- src/footer.js
+++ src/footer.js
@@ -5,10 +5,10 @@ function Footer() {
         <div className="footer">
             <div className="social-media-container">
                 <h2>Connect with us on Social Media to discover new recipes</h2>
-                <a href='#' target="_blank"><i id="instagram" className="fa-brands fa-instagram"></i></a>
-                <a href='#' target="_blank"><i id="facebook" className="fa-brands fa-square-facebook"></i></a>
-                <a href='#' target="_blank"><i id="twitter" className="fa-brands fa-square-twitter"></i></a>
-                <img src={logo_white} className="logo-white"></img>
+                <a href='https://www.instagram.com/' target="_blank"><i id="instagram" className="fa-brands fa-instagram"></i></a>
+                <a href='https://en-gb.facebook.com/' target="_blank"><i id="facebook" className="fa-brands fa-square-facebook"></i></a>
+                <a href='https://twitter.com/' target="_blank"><i id="twitter" className="fa-brands fa-square-twitter"></i></a>
+                <img src={logo_white} className="logo-white" alt='get baking logo in white'></img>
             </div>
         </div>
     );
