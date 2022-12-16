fbd086a05d16fd46a049cc2b17b117efe49209e9
TobyDyson123 <tobyrdyson@ntlworld.com>
2022-12-16 16:45:39 +0000

    added images to home pages

--- src/carousel.js
+++ src/carousel.js
@@ -3,6 +3,7 @@ import cake from './images/cake.jpg';
 import bread from './images/bread.jpg';
 import Axios from "axios";
 import { useEffect, useState } from 'react';
+import data from './recipes.json';
 
 function RecentRecipesCarousel() {
   const [recipes, getRecipes] = useState('');
@@ -28,39 +29,39 @@ function RecentRecipesCarousel() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={cake}
+          src={data[0].images[0]}
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>{data[0].name}</h3>
+          <p>{data[0].description}</p>
           <button>Show me more!</button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={cake}
+          src={data[1].images[0]}
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>{data[1].name}</h3>
+          <p>{data[1].description}</p>
           <button>Show me more!</button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={bread}
+          src={data[2].images[0]}
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>{data[2].name}</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            {data[2].description}
           </p>
           <button>Show me more!</button>
         </Carousel.Caption>
@@ -122,39 +123,39 @@ function SeasonalRecipesCarousel() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={bread}
+          src={data[2].images[0]}
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>{data[2].name}</h3>
+          <p>{data[2].description}</p>
           <button>Show me more!</button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={cake}
+          src={data[0].images[0]}
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>{data[0].name}</h3>
+          <p>{data[0].description}</p>
           <button>Show me more!</button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={bread}
+          src={data[3].images[0]}
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>{data[3].name}</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            {data[3].description}
           </p>
           <button>Show me more!</button>
         </Carousel.Caption>
--- src/main-content.js
+++ src/main-content.js
@@ -5,6 +5,7 @@ import { RecentRecipesCarousel, PopularDemandCarousel, SeasonalRecipesCarousel }
 import Card from './card.js';
 import cake from './images/cake.jpg';
 import bread from './images/bread.jpg';
+import pastry from './images/pastry.jpg';
 import Axios from "axios";
 
 function MainContent() {
@@ -20,7 +21,6 @@ function MainContent() {
 
     return (
         <div className="main-content">
-            <button onClick={getRecipes}>Get Recipes</button>
             <div className="recipes-carousel-container">
                 <h1 className='recipes-title'>Recent Recipes</h1>
                 <RecentRecipesCarousel />
@@ -43,7 +43,7 @@ function MainContent() {
                     In ante ex, congue et quam cursus, vestibulum dictum mi. Pellentesque cursus risus risus, at mollis nisi eleifend eget. 
                     Donec blandit justo ac vestibulum mattis. Aliquam nec scelerisque nulla, ac viverra lacus. Etiam eu ornare metus. 
                     Morbi ac tristique justo. Proin placerat interdum leo ac efficitur." />
-            <Card title="Build Custom Recipes" src={cake} alt="picture of cake"
+            <Card title="Build Custom Recipes" src={pastry} alt="picture of pastry"
                     content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non ex a quam commodo interdum laoreet eu orci. 
                     In ante ex, congue et quam cursus, vestibulum dictum mi. Pellentesque cursus risus risus, at mollis nisi eleifend eget. 
                     Donec blandit justo ac vestibulum mattis. Aliquam nec scelerisque nulla, ac viverra lacus. Etiam eu ornare metus. 
