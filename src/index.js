651f043a48163164f34ad113f744e97e6ff8fb2a
TobyDyson123 <tobyrdyson@ntlworld.com>
2022-12-18 17:40:01 +0000

    added log test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
+console.log("HI");
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
