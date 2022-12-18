bddfd7371c7816d7d77d96bac59391cd64ce6d7a
TobyDyson123 <tobyrdyson@ntlworld.com>
2022-12-18 18:45:11 +0000

    added routes instead of switch

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import Home from './Home.js';
 import ExploreRecipes from './ExploreRecipes';
 import Leftovers from './Leftovers';
 import CustomRecipes from './CustomRecipes';
-import { Routes, Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 // https://get-baking.free.beeceptor.com/recipes
 
 function App() {
   return (
     <div className="App">
-      <Switch>
+      <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/explorerecipes" element={<ExploreRecipes />} />
         <Route path="/leftovers" element={<Leftovers />} />
         <Route path="/customrecipes" element={<CustomRecipes />} />
-      </Switch>
+      </Routes>
     </div>
   );
 }
