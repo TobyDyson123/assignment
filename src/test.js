3cfeef771cdd5dd891927f9beeece64551797e6f
TobyDyson123 <tobyrdyson@ntlworld.com>
2022-12-19 18:01:45 +0000

    added link containers

--- src/test.js
+++ src/test.js
@@ -1,18 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky='top'>
       <Container>
-        <Navbar.Brand href="https://tobydyson123.github.io/interactive-web/#/">Get Baking!</Navbar.Brand>
+      <LinkContainer><Navbar.Brand href="https://tobydyson123.github.io/interactive-web/#/">Get Baking!</Navbar.Brand></LinkContainer>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="https://tobydyson123.github.io/interactive-web/#/explorerecipes">Explore Recipes</Nav.Link>
-            <Nav.Link href="https://tobydyson123.github.io/interactive-web/#/leftovers">Use up your Leftovers</Nav.Link>
-            <Nav.Link href="https://tobydyson123.github.io/interactive-web/#/customrecipes">Build Custom Recipes</Nav.Link>
+            <LinkContainer><Nav.Link href="https://tobydyson123.github.io/interactive-web/#/explorerecipes">Explore Recipes</Nav.Link></LinkContainer>
+            <LinkContainer><Nav.Link href="https://tobydyson123.github.io/interactive-web/#/leftovers">Use up your Leftovers</Nav.Link></LinkContainer>
+            <LinkContainer><Nav.Link href="https://tobydyson123.github.io/interactive-web/#/customrecipes">Build Custom Recipes</Nav.Link></LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
