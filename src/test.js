6f35fe1552a652b1be2eff97312e3db66253be29
TobyDyson123 <tobyrdyson@ntlworld.com>
2022-12-19 16:00:08 +0000

    edited paths

--- src/test.js
+++ src/test.js
@@ -6,13 +6,13 @@ function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky='top'>
       <Container>
-        <Navbar.Brand href="https://tobydyson123.github.io/interactive-web">Get Baking!</Navbar.Brand>
+        <Navbar.Brand href="/interactive-web">Get Baking!</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="https://tobydyson123.github.io/interactive-web/#/explorerecipes">Explore Recipes</Nav.Link>
-            <Nav.Link href="https://tobydyson123.github.io/interactive-web/#/leftovers">Use up your Leftovers</Nav.Link>
-            <Nav.Link href="https://tobydyson123.github.io/interactive-web/#/customrecipes">Build Custom Recipes</Nav.Link>
+            <Nav.Link href="/explorerecipes">Explore Recipes</Nav.Link>
+            <Nav.Link href="/leftovers">Use up your Leftovers</Nav.Link>
+            <Nav.Link href="/customrecipes">Build Custom Recipes</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
