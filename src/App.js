d11de74ed78c025902cd1b347ea5b40f1717012f
TobyDyson123 <tobyrdyson@ntlworld.com>
2022-12-19 16:55:10 +0000

    changed paths

--- src/App.js
+++ src/App.js
@@ -8,19 +8,21 @@ import Home from './Home.js';
 import ExploreRecipes from './ExploreRecipes';
 import Leftovers from './Leftovers';
 import CustomRecipes from './CustomRecipes';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 // https://get-baking.free.beeceptor.com/recipes
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="interactive-web/explorerecipes" element={<ExploreRecipes />} />
-        <Route path="/leftovers" element={<Leftovers />} />
-        <Route path="/customrecipes" element={<CustomRecipes />} />
-      </Routes>
+      <HashRouter basename="/interactive-web">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/explorerecipes" element={<ExploreRecipes />} />
+          <Route path="/leftovers" element={<Leftovers />} />
+          <Route path="/customrecipes" element={<CustomRecipes />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/interactive-web">
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
 
